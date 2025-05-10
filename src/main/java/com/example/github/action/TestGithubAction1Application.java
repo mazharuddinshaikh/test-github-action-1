243ecd3c21c6e2d3842247795f5f0ad70b7c5e1c
ShaikhMazharuddin <mazhar.s2k18@gmail.com>
2025-05-10 16:17:57 +0530

    initial commit

---
+++ src/main/java/com/example/github/action/TestGithubAction1Application.java
@@ -0,0 +1,13 @@
+package com.example.github.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubAction1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGithubAction1Application.class, args);
+	}
+
+}
