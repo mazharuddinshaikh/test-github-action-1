715618fc30797f91e957ea2ab88aed7bc3b05df1
ShaikhMazharuddin <mazhar.s2k18@gmail.com>
2025-05-10 16:33:28 +0530

    print statement added

--- src/main/java/com/example/github/action/TestGithubAction1Application.java
+++ src/main/java/com/example/github/action/TestGithubAction1Application.java
@@ -1,13 +1,20 @@
 package com.example.github.action;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestGithubAction1Application {
+public class TestGithubAction1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestGithubAction1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Application started");
+		
 	}
 
 }
